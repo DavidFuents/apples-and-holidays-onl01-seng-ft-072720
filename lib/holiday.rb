0cd0dd1741b7f60e64d083a66ce62c4d25c27bf1
David Fuentes <davidf0827@gmail.com>
2020-08-03 09:13:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,7 +86,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.map do |season, holiday| 
     holiday.map do |holiday, item|
-     result = holiday if item.include?("BBQ")
+     holiday if item.include?("BBQ")
     end
   end
   .flatten.compact
